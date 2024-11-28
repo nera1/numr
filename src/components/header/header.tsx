cb55c3fd2a792f97baebc6f8001d0008181bb21d
nera1 <rustydoor4728@gmail.com>
2024-11-28 11:59:35 +0900

    feature: update palette

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { FunctionComponent } from "react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 import { Github } from "lucide-react";
 
-import styles from "@/styles/header/header.module.scss";
 import Search from "./search";
+import Logo from "../svg/logo";
+
+import styles from "@/styles/header/header.module.scss";
 
 const Header: FunctionComponent<any> = () => {
   return (
     <header className={styles["header"]}>
       <div className={styles["container"]}>
-        <div className={styles["left"]}></div>
+        <div className={styles["left"]}>
+          <Link href={"/"} className={styles["home"]}>
+            <Logo />
+          </Link>
+        </div>
         <div className={styles["center"]}></div>
         <div className={styles["right"]}>
           <Search />
