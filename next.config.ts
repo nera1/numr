f0983c0a7ed315572d8254feb2e3bb0cb8736e1a
nera1 <rustydoor4728@gmail.com>
2024-12-11 18:51:55 +0900

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  reactStrictMode: false,
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
   },
+  basePath: "/numr", // 리포지토리 이름 추가
+  assetPrefix: "/numr", // 정적 파일 경로 설정
 };
 
 export default nextConfig;
