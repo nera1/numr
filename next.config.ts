7b3133e9fb8b59ca7064da626d33a2d43603e879
nera1 <rustydoor4728@gmail.com>
2024-12-12 18:36:39 +0900

    test: update order-select 2

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,11 @@ const debug = process.env.NODE_ENV === "production";
 const repository = "numr";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // Static export mode
   reactStrictMode: false,
-  sassOptions: {
-    silenceDeprecations: ["legacy-js-api"],
-  },
   assetPrefix: debug ? `/${repository}/` : "",
   basePath: debug ? `/${repository}` : "",
-  trailingSlash: true, // 모든 경로에 슬래시 추가
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-    };
-  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
