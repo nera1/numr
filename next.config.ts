ac8e1887cdbba170535783292b96e0d85cc1ee89
nera1 <rustydoor4728@gmail.com>
2024-12-12 09:38:51 +0900

    test: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const debug = process.env.NODE_ENV === "production";
+const repository = "numr";
+
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: false,
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
   },
+  assetPrefix: debug ? `/${repository}/` : "",
+  basePath: debug ? `/${repository}` : "",
 };
 
 export default nextConfig;
