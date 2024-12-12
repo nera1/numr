6b7ba779c19bd383acb41b888661c06098c4ad7f
nera1 <rustydoor4728@gmail.com>
2024-12-12 10:06:38 +0900

    test: update order select

--- src/components/order-select/order-select.tsx
+++ src/components/order-select/order-select.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FunctionComponent, useState, Suspense, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -45,7 +45,7 @@ const OrderSelect: FunctionComponent = () => {
   ];
 
   const searchParams = useSearchParams();
-  //const router = useRouter();
+  const router = useRouter();
 
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(orders[0].value);
@@ -63,6 +63,7 @@ const OrderSelect: FunctionComponent = () => {
       updatedQueryString = new URLSearchParams(newQueryString).toString();
     }
     console.log(updatedQueryString);
+    router.push(`?${updatedQueryString}`);
   }, [value]);
 
   return (
