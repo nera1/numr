75d68a2e7c1a53b9a22ed5804bd760810683401f
ekd594ff <ekd594ff@gmail.com>
2024-12-16 14:28:27 +0900

    feature: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const debug = process.env.NODE_ENV === "production";
-const repository = "numr";
+const repository = "sumr";
 
 const nextConfig: NextConfig = {
   output: "export", // Static export mode
